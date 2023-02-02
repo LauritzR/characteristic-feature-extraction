fde34377297fddb668f0aa76af324e92ec8e3ca5
Lauritz Rasbach <35218654+LauritzR@users.noreply.github.com>
2023-02-02 16:41:48 +0100

    Update dbscan.py

--- src/no_label_pfa/dbscan.py
+++ src/no_label_pfa/dbscan.py
@@ -3,31 +3,29 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
+# dbscan paramters
+# path_embedding: string path to the embedding output file (e.g. tsne or umap)
+# eps: maximum distance between two samples for them to be considered neighbours
+# min_samples: number of samples in a neighborhood for a point to be considered as a core point
+# plot: flag for visual output
 
-def dbscan(path_embedded, path_original, path_pfs="principal_features0.txt", eps=2, min_samples=15):
-    X = pd.read_csv(path_embedded, sep=',', header=None).to_numpy().T
+def dbscan(path_embedding, eps=2, min_samples=15, plot=True):
+
+    X = pd.read_csv(path_embedding, sep=',', header=None).to_numpy().T
 
     clustering = DBSCAN(eps=eps, min_samples=min_samples).fit_predict(X)
 
     np.savetxt("dbscan_labels.csv", clustering, delimiter=",")
 
-    with open(path_pfs) as f:
-        pfs = f.readlines() 
-    pfs = [int(x[:len(x)-2]) for x in pfs]
-
-    pf_data = pd.read_csv(path_original, sep=',', header=None).to_numpy()[pfs].T
-
-    data = np.c_[clustering, pf_data]
-
-    np.savetxt("dbscan_output.csv", data.T, delimiter=",")
-
-    for i in np.unique(clustering):
-        mask = clustering == i
-        plt.scatter(X[mask, 0], X[mask, 1],  label=i)
-
     values, counts = np.unique(clustering, return_counts=True)
 
-    for v,c in zip(values, counts):
-        print("Label {}: {}".format(v,c))
-    plt.legend()
-    plt.show()
+    for v, c in zip(values, counts):
+        print("Label {}: {}".format(v, c))
+
+    if plot:
+        for i in np.unique(clustering):
+            mask = clustering == i
+            plt.scatter(X[mask, 0], X[mask, 1],  label=i)
+
+        plt.legend()
+        plt.show()
