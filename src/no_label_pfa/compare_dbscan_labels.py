a075e3ad048bb07bcec8cb6b77dae8bd3027fe54
Lauritz Rasbach <35218654+LauritzR@users.noreply.github.com>
2023-02-08 17:15:44 +0100

    Create compare_dbscan_labels.py

---
+++ src/no_label_pfa/compare_dbscan_labels.py
@@ -0,0 +1,28 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# compare_dbscan_labels paramters
+# path_comparison_labels: string path to the file containing labels to compare
+def compare_dbscan_labels(path_comparison_labels):
+    comparison_labels = pd.read_csv(
+        path_comparison_labels, sep=',', header=None).to_numpy().flatten()
+    clustering = pd.read_csv("dbscan_labels.csv", sep=',',
+                             header=None).to_numpy().flatten()
+
+    cluster_bins = []
+    for l in np.unique(clustering):
+        tmp = [comparison_labels[i]
+               for i in range(len(clustering)) if clustering[i] == l]
+        cluster_bins.append(tmp)
+
+    plt.ylabel("Count", fontsize=12)
+    plt.xlabel("DBSCAN Prediction", fontsize=12)
+
+    plt.xticks(range(len(np.unique(clustering))))
+
+    n, bins, patches = plt.hist(
+        cluster_bins, histtype='barstacked', label=np.unique(comparison_labels))
+
+    plt.legend(title="Comparison labels", fancybox=True)
+    plt.show()
