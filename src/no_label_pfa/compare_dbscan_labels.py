199d5ee85062fca78f12ceea3f22826d7963166d
Lauritz Rasbach <35218654+LauritzR@users.noreply.github.com>
2023-02-23 12:40:31 +0100

    feat: dbscan cluster filter

--- src/no_label_pfa/compare_dbscan_labels.py
+++ src/no_label_pfa/compare_dbscan_labels.py
@@ -5,20 +5,24 @@ import matplotlib.pyplot as plt
 # compare_dbscan_labels paramters
 # path_comparison_labels: string path to the file containing labels to compare
 # path_dbscan_labels: string path to the file containing the labels found by dbscan, optional
-def compare_dbscan_labels(path_comparison_labels, path_dbscan_labels="dbscan_labels.csv"):
+# clusters: list of dbscan clusters to be considered in the calculation. If empty, all clusters are considered
+def compare_dbscan_labels(path_comparison_labels, path_dbscan_labels="dbscan_labels.csv", clusters=[]):
     comparison_labels = pd.read_csv(
         path_comparison_labels, sep=',', header=None).to_numpy().flatten()
     clustering = pd.read_csv(path_dbscan_labels, sep=',',
                              header=None).to_numpy().flatten()
 
+    if len(clusters) == 0:
+        clusters = np.unique(clustering)
+
     cluster_bins = []
 
-    for l in np.unique(clustering):
+    for l in clusters:
         tmp = [int(l)]
         for cl in np.unique(comparison_labels):
             count = 0
             for i in range(len(comparison_labels)):
-                if clustering[i] == l and comparison_labels[i] == cl:
+                if clustering[i] == l and comparison_labels[i] == cl and clustering[i] in clusters:
                     count += 1
             tmp.append(count)
 
