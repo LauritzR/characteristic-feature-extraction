edc2dd53693bc8a01041dc3a09383e4db3aa278e
Tib20 <87315574+Tib20@users.noreply.github.com>
2023-07-10 17:17:21 +0200

    Update split_data.py

--- src/no_label_pfa/split_data.py
+++ src/no_label_pfa/split_data.py
@@ -6,8 +6,8 @@ import pandas as pd
 # n_splits: number of splits/output files to generate
 # path_labels: string path to the file containing lables for the dataset (e.g. the dbscan output file)
 # clusters: list of clusters to be considered in the calculation. If empty, all clusters are considered
-# cluster_ratio: ratio between clusters for the splits. Overwrites n_splits
-def split_data(path_original_data, n_splits, path_labels="dbscan_labels.csv",clusters=[], cluster_ratio=[]):
+# number_per_cluster: number of samples per cluster in each splitted file. Overwrites n_splits
+def split_data(path_original_data, n_splits, path_labels="dbscan_labels.csv",clusters=[], number_per_cluster=[]):
 
     data = pd.read_csv(path_original_data, sep=',', header=None).to_numpy().T
     clustering = pd.read_csv(path_labels, sep=',', header=None).to_numpy()
@@ -25,21 +25,21 @@ def split_data(path_original_data, n_splits, path_labels="dbscan_labels.csv",clu
     _, counts = np.unique(data.T[0], return_counts=True)
 
     split_size = np.ceil((len(data.T)/n_splits)/len(clusters))
-    if len(cluster_ratio):
-        n_splits = int(np.ceil(len(data.T)/np.sum(cluster_ratio)))
+    if len(number_per_cluster):
+        n_splits = int(np.ceil(len(data.T)/np.sum(number_per_cluster)))
     else:
-        cluster_ratio = [int(split_size) for i in range(len(clusters))]
+        number_per_cluster = [int(split_size) for i in range(len(clusters))]
 
     for i in range(n_splits):
         print("Split "+str(i))
         split = []
         for c in clusters:
-            for n in range(cluster_ratio[int(c)]):
+            for n in range(number_per_cluster[int(c)]):
 
-                if (i*cluster_ratio[int(c)]) + n >= counts[int(c)]:
+                if (i*number_per_cluster[int(c)]) + n >= counts[int(c)]:
                     print("No more datapoints for cluster "+str(c))
                 else:
-                    split.append(data.values.T[(i*cluster_ratio[int(c)]) + n + np.sum(counts[:int(c)])]) 
+                    split.append(data.values.T[(i*number_per_cluster[int(c)]) + n + np.sum(counts[:int(c)])]) 
 
         split = np.array(split)        
         np.savetxt("split_"+str(i)+"_y.csv", split.T[0], delimiter=",")
